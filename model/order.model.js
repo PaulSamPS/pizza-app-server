488b5abc1518ca3a63bccdaccd5a59476d3e23f6
PaulSamPS <psamoylenko2@gmail.com>
2023-02-16 00:07:40 +0300

    order model

--- model/order.model.js
+++ model/order.model.js
@@ -2,8 +2,21 @@ const { Schema, model } = require('mongoose')
 
 const OrderSchema = new Schema(
   {
-    pizza: { type: Schema.Types.ObjectId, ref: 'Pizza' },
-    product: { type: Schema.Types.ObjectId, ref: 'Product' },
+    products: [
+      {
+        pizza: { type: Schema.Types.ObjectId, ref: 'Pizza' },
+        product: { type: Schema.Types.ObjectId, ref: 'Product' },
+        size: { type: String },
+        dough: { type: String },
+        qty: { type: Number },
+        price: { type: Number },
+      },
+    ],
+    address: { type: String },
+    status: { type: String },
+    orderNumber: { type: String },
+    totalPrice: { type: Number },
+    paymentMethod: { type: String },
   },
   { timestamps: true }
 )
